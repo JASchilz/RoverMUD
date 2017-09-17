312124a1f4bee8438bc479c652a4b095906e7438
Joseph Schilz <joseph@schilz.org>
2017-09-16 21:33:17 -0700

    Stub out module tests for basics.

---
+++ basics/test/module-test.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+
+class ModuleTest(TestCase):
+
+    def test_character_attach_attachment(self):
+        self.fail("Test unwritten")
+
+    def test_container_containment(self):
+        self.fail("Test unwritten")
