bcbaa3495eb77bcabca993f1d8315c6dbc79cda7
Joseph Schilz <joseph@schilz.org>
2017-09-26 07:58:28 -0700

    Write character attachment test.

--- basics/test/module-test.py
+++ basics/test/module-test.py
@@ -1,10 +1,23 @@
 from unittest import TestCase
 
+from basics import BaseCharacter
+from basics import BaseAttachment
+
 
 class ModuleTest(TestCase):
 
     def test_character_attach_attachment(self):
-        self.fail("Test unwritten")
+        character = BaseCharacter().save()
+        attachment = BaseAttachment().save()
+
+        # Attachment should not be among the character's attachments
+        self.assertNotIn(attachment.id, character.attachments())
+
+        # Attach the attachment
+        character.attach(attachment)
+
+        # Attachment should be among the character's attachments
+        self.assertIn(attachment.id, character.attachments())
 
     def test_container_containment(self):
         self.fail("Test unwritten")
